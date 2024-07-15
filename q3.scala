8746c1935d1d922a76d48d9fc5e32fa5e716e6fa
nive19s <sothilingamnivethan19@gmail.com>
2024-07-16 02:15:11 +0530

    modified

--- q3.scala
+++ q3.scala
@@ -1,22 +1,23 @@
 import scala.io.StdIn.readLine
 import scala.io.StdIn.readInt
 
-object fabbinocci {
+object fibonacci {
     def main(args: Array[String]): Unit = {
         
-        def fabbinocci_series(n:Int): Int = n match {
+        def fibonacci_series(n:Int): Int = n match {
             case x if (x==0)=> 0
             case x if(x ==1) => 1
-            case _ => fabbinocci_series(n-1)+ fabbinocci_series(n-2)
+            case _ => fibonacci_series(n-1)+ fibonacci_series(n-2)
+            
         }
 
-        def fabbinnoci_print(x:Int): Unit ={
-            if (x>0) fabbinnoci_print(x-1)
-            print(s"${fabbinocci_series(x)} ,")
+        def print_fibonacci(x:Int): Unit ={
+            if (x>0) print_fibonacci(x-1)
+            print(s"${fibonacci_series(x)} ,")
         }
-        print("Enter the number to fabbinnoci: ")
+        print("Enter the number: ")
         var a = readInt()
-        println(s"The fibonacci of ${a} is : ${fabbinocci_series(a)}")
-        fabbinnoci_print(a)
+        println(s"First ${a} fibonacci numbers  : ")
+        print_fibonacci(a)
     }
 }
